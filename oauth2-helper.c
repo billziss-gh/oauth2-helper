a558a3c49900e76a977171f62d0e98fc7c4fb679
Bill Zissimopoulos <billziss@navimatics.com>
2018-02-17 02:51:23 +0000

    initial windows implementation

--- oauth2-helper.c
+++ oauth2-helper.c
@@ -7,6 +7,13 @@
 #include <stdarg.h>
 
 #if defined(_WIN64) || defined(_WIN32)
+#include <windows.h>
+#define strcpy(d, s)                    lstrcpyA(d, s)
+#define strlen(s)                       lstrlenA(s)
+#define malloc(s)                       HeapAlloc(GetProcessHeap(), 0, s)
+#define free(p)                         ((p) ? (void)HeapFree(GetProcessHeap(), 0, p) : (void)0)
+#define STDOUT_FILENO                   (intptr_t)GetStdHandle(STD_OUTPUT_HANDLE)
+#define STDERR_FILENO                   (intptr_t)GetStdHandle(STD_ERROR_HANDLE)
 #elif defined(__CYGWIN__)
 #elif defined(__APPLE__)
 #include <CoreFoundation/CoreFoundation.h>
@@ -23,10 +30,18 @@ enum
     E_TIMEOUT                           = 'T',
 };
 
+#if defined(_WIN64) || defined(_WIN32)
+int write(intptr_t fd, const void *buf, size_t len)
+{
+    DWORD BytesTransferred;
+    return WriteFile((HANDLE)fd, buf, len, &BytesTransferred, 0) ? BytesTransferred : -1;
+}
+#endif
+
 void err(int result, const char *fmt, ...)
 {
     va_list ap;
-    char buf[1024];
+    char buf[64+1024];
     size_t len;
 
     switch (result)
@@ -47,21 +62,32 @@ void err(int result, const char *fmt, ...)
     len = strlen(buf);
 
     va_start(ap, fmt);
+#if defined(_WIN64) || defined(_WIN32)
+    wvsprintf(buf + len, fmt, ap);
+#else
     vsnprintf(buf + len, sizeof buf - len, fmt, ap);
+#endif
     va_end(ap);
 
-    write(2, buf, strlen(buf));
+    write(STDERR_FILENO, buf, strlen(buf));
 }
 
 int browser(const char *url)
 {
+    int result = E_BROWSER;
 #if defined(_WIN64) || defined(_WIN32)
+    if (!ShellExecuteA(0, "open", url, 0, 0, SW_SHOWNORMAL))
+    {
+        err(result, "ShellExecuteA: %d\n", GetLastError());
+        goto exit;
+    }
+    result = 0;
+exit:
 #elif defined(__CYGWIN__)
 #elif defined(__APPLE__)
     CFStringRef urlstr = 0;
     CFURLRef urlref = 0;
     OSStatus status;
-    int result = E_BROWSER;
     urlstr = CFStringCreateWithCString(0, url, kCFStringEncodingUTF8);
     if (0 == urlstr)
     {
@@ -86,11 +112,9 @@ exit:
         CFRelease(urlref);
     if (0 != urlstr)
         CFRelease(urlstr);
-    return result;
 #elif defined(__linux__)
-#else
-#error Unknown platform
 #endif
+    return result;
 }
 
 int main(int argc, char *argv[])
@@ -98,3 +122,34 @@ int main(int argc, char *argv[])
     browser(argv[1]);
     return 0;
 }
+
+#if defined(_WIN64) || defined(_WIN32)
+void mainCRTStartup(void)
+{
+    DWORD Argc;
+    PWSTR *Argv;
+    int Length;
+    char **argv, *argp, *argendp;
+
+    Argv = CommandLineToArgvW(GetCommandLineW(), &Argc);
+    if (0 == Argv)
+        ExitProcess(GetLastError());
+    
+    Length = 0;
+    for (DWORD I = 0; Argc > I; I++)
+        Length += WideCharToMultiByte(CP_UTF8, 0, Argv[I], -1, 0, 0, 0, 0);
+    argv = malloc((Argc + 1) * sizeof(char *) + Length);
+    argp = (char *)argv + (Argc + 1) * sizeof(char *);
+    argendp = argp + Length;
+    for (DWORD I = 0; Argc > I; I++)
+    {
+        argv[I] = argp;
+        argp += WideCharToMultiByte(CP_UTF8, 0, Argv[I], -1, argp, argendp - argp, 0, 0);
+    }
+    argv[Argc] = 0;
+
+    LocalFree(Argv);
+
+    ExitProcess(main(Argc, argv));
+}
+#endif
